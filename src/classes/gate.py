d42c9cc37188c2418ede55e7425b1db6551e7e82
hugo.mayer <hugo@mayer@epita.fr>
2024-10-14 14:15:13 +0200

    feature : control_class

--- src/classes/gate.py
+++ src/classes/gate.py
@@ -1,4 +1,5 @@
 from mpqp.gates import Gate as MPQP_gate
+from mpqp.gates import ControlledGate
 from typing import List, Optional
 
 
@@ -6,6 +7,10 @@ class Gate:
     def __init__(self, gate: MPQP_gate):
         self.targets = gate.targets
         self.label = gate.label
+        if isinstance(gate, ControlledGate):
+            self.control = gate.controls
+        else:
+            self.control = None
         self.gate = gate
 
     def __repr__(self):

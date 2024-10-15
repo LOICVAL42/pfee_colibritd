2e7fafe13ae30a8659feef96c6a9721f8bcaa17a
Michalon Loïc <loic.michalon@epita.fr>
2024-10-15 11:05:37 +0200

    [fix]: netlist_to_qCircuit

--- src/converters/netlist_converters.py
+++ src/converters/netlist_converters.py
@@ -7,4 +7,4 @@ def qCircuit_to_netlist(qCircuit: QCircuit) -> np.ndarray[MPQP_gate]:
     return [Gate(gate) for gate in qCircuit.instructions if isinstance(gate, MPQP_gate)]
 
 def netlist_to_qCircuit(netlist: np.ndarray[MPQP_gate]) -> QCircuit:
-    return QCircuit(netlist)
+    return QCircuit([gate.gate for gate in netlist])
